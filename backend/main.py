44b92eec9b002f53841a2ffcfc1dbcbc130e9fdb
Guilherme Freire <guitenan@hotmail.com>
2025-09-28 15:46:30 -0300

    fix: Correcao do Procfile e configuracao para Render

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     
     import os
     
-    host = os.getenv("HOST", "127.0.0.1")
+    host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", 8000))
     
     uvicorn.run(
